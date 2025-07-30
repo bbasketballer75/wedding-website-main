47adb64b46a7760e8f9c15ccd0f59ef3b54b72eb
Austin <bbasketballer75@gmail.com>
2025-07-29 20:28:58 -0400

    Remove unused jest.config.cjs to fix Jest config conflict in CI

--- backend/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.js$': ['babel-jest', { configFile: './babel.config.mjs' }],
-  },
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-  moduleNameMapper: {},
-};
